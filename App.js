4ea38b20eaaa70b6d9af3363a92e691907de1136
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2020-12-31 22:17:30 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,48 @@
 import React, { Component } from "react";
 import "./styles.css";
-import Person from "./Person";
+import Person from "./Component/Person";
 
 class App extends Component {
   state = {
-    person: [{ name: "akash" }]
+    person: [
+      { name: "akash" },
+    {name:"hero"},
+    {name:"boss"}
+  ]
   };
 
-  switchHandler = () => {
+  switchHandler = (newname) => {
     this.setState({
-      person: [{ name: "the great" }]
-    });
-  };
+      person: [{ name: newname },
+    {name:"akshy"},
+    {name:"kumar"}
+  ]
+    })
+  }
+
+  changeHandler = (event) =>{
+    this.setState({
+      person: [{ name:event.target.value},
+    {name:"akshy"},
+    {name:"kumar"}
+  ]
+    })
+    
+
+
+
+  }
   render() {
     return (
       <div>
-        <h3> hi babe </h3>
+        <h3> hello </h3>
 
-        <button onClick={this.switchHandler}>Click me</button>
-        <Person name={this.state.person[0].name} />
+        <button onClick={()=> this.switchHandler("yamma")}>Click me</button>
+        <Person name={this.state.person[0].name} 
+          click={()=>this.switchHandler("jimbalakadi")}
+          change = {this.changeHandler}> hobbies:I love computers </Person>
+        <Person name={this.state.person[1].name} />
+        <Person name={this.state.person[2].name} />
       </div>
     );
   }
